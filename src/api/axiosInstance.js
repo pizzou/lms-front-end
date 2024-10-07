c67391f794d62a602669658af3ff7ff4bbe46651
pizzou <pmpumuropizzou@gmail.com>
2024-10-07 19:18:28 +0200

    001 commit

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://lms-server-nrfz.onrender.com',
 });
 
 // Interceptor to attach token for authenticated requests
